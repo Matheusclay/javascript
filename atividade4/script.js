58a90224b417aeb43141da9ba628a81361aa3c8e
Matheus Henrique da Silva <matheeus.silva9@hotmail.com>
2024-08-18 23:30:08 -0300

    finish

--- atividade4/script.js
+++ atividade4/script.js
@@ -1,13 +1,18 @@
 function verificarTipoDado() {
-
-let dados = prompt("Por favor, insira um dado:");
-        let verificacao = confirm("Você deseja verificar o tipo do dado informado?");
-        if (verificacao) {
-            alert ("O tipo do dado informado é: " + typeof dados);
-        } else {
-            alert ("Operação cancelada pelo usuário.");
+    let dados = prompt("Por favor, insira um dado:");
+    let verificacao = confirm("Você deseja verificar o tipo do dado informado?");
+    
+    if (verificacao) {
+        if (!isNaN(parseFloat(dados)) ) {
+            dados = parseFloat(dados);
+        } else if (dados.toLowerCase() === "true" || dados.toLowerCase() === "false") {
+            dados = (dados.toLowerCase() === "true");
         }
-
+        
+        alert("O tipo do dado informado é: " + typeof dados);
+    } else {
+        alert("Operação cancelada pelo usuário.");
+    }
 }
 
 verificarTipoDado();
