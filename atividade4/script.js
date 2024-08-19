5681379c78b7e3a74f663fc39d462fc3659b4b8f
Matheus Henrique da Silva <matheeus.silva9@hotmail.com>
2024-08-18 23:09:58 -0300

    fianlizando o ex 4

--- atividade4/script.js
+++ atividade4/script.js
@@ -1,5 +1,13 @@
-function verificar_tipo() {
+function verificarTipoDado() {
+
+let dados = prompt("Por favor, insira um dado:");
+        let verificacao = confirm("Você deseja verificar o tipo do dado informado?");
+        if (verificacao) {
+            alert ("O tipo do dado informado é: " + typeof dados);
+        } else {
+            alert ("Operação cancelada pelo usuário.");
+        }
 
 }
-var tipo = 
-verificar_tipo();
+
+verificarTipoDado();
